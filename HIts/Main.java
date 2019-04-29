61f808b748fc8ef8bd9f699964e9b1bb9e3de29c
virajdoshi <44646580+virajdoshi@users.noreply.github.com>
2019-04-29 23:07:24 +0530

    Update Main.java

--- HIts/Main.java
+++ HIts/Main.java
@@ -2,24 +2,6 @@ import java.util.Scanner;
 
 public class Main
 {
-    public static void bubbleSort(double[] arr) 
-    {  
-        int n = arr.length;  
-        double temp = 0;  
-        for(int i=0; i < n; i++)
-        {  
-            for(int j=1; j < (n-i); j++)
-            {  
-                if(arr[j-1] > arr[j])
-                {  
-                    //swap elements  
-                    temp = arr[j-1];  
-                    arr[j-1] = arr[j];  
-                    arr[j] = temp;  
-                }  
-            }  
-        }
-    }
     public static void main(String[] args)
     {
         double[][] A, ATranspose;
